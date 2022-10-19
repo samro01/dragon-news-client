b0957e319bae7bebd8df0d1b0fa185a35ee9abd5
samro01 <srozario22@gmail.com>
2022-10-19 11:10:32 +0200

    first commit

---
+++ src/Pages/Shared/LeftSideNav/LeftSideNav.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LeftSideNav = () => {
+    return (
+        <div>
+            <h2>Left side Nav</h2>
+        </div>
+    );
+};
+
+export default LeftSideNav;
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,34 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBFHK7b452Ggml9SrYuYDsr8FZp0kQ4leM",
+    authDomain: "dragon-news-a3835.firebaseapp.com",
+    projectId: "dragon-news-a3835",
+    storageBucket: "dragon-news-a3835.appspot.com",
+    messagingSenderId: "147789795772",
+    appId: "1:147789795772:web:87e7a65caa7e7950e49bb9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBFHK7b452Ggml9SrYuYDsr8FZp0kQ4leM",
+    authDomain: "dragon-news-a3835.firebaseapp.com",
+    projectId: "dragon-news-a3835",
+    storageBucket: "dragon-news-a3835.appspot.com",
+    messagingSenderId: "147789795772",
+    appId: "1:147789795772:web:87e7a65caa7e7950e49bb9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
