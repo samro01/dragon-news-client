26e2c5a9e5e64d3c496f7d7e55a1e51fa3b229ef
samro01 <srozario22@gmail.com>
2022-10-19 15:36:56 +0200

    button and fond awesome added

--- src/Pages/Shared/RightSideNav/RightSideNav.js
+++ src/Pages/Shared/RightSideNav/RightSideNav.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import { FaGoogle, FaGithub, FaFacebook, FaTwitter, FaWhatsapp, FaTwitch } from "react-icons/fa";
+import ListGroup from 'react-bootstrap/ListGroup';
 
 const RightSideNav = () => {
     return (
         <div>
-            <h2 >Right side nav </h2>
+            <ButtonGroup vertical>
+
+                <Button className="mb-2" variant="outline-primary"> <FaGoogle></FaGoogle> Login with Google</Button>
+                <Button variant="outline-dark"> <FaGithub></FaGithub> Login ith Github</Button>
+            </ButtonGroup>
+            <div>
+                <h5>Find us on</h5>
+                <ListGroup>
+                    <ListGroup.Item className="mb-2"><FaFacebook></FaFacebook> Facebook</ListGroup.Item>
+                    <ListGroup.Item className="mb-2"><FaTwitter></FaTwitter> Twitter</ListGroup.Item>
+                    <ListGroup.Item className="mb-2"><FaWhatsapp></FaWhatsapp> WhatsApp</ListGroup.Item>
+                    <ListGroup.Item className="mb-2"> <FaTwitch></FaTwitch> Twitch</ListGroup.Item>
+                </ListGroup>
+            </div>
         </div >
     );
 };
