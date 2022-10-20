424b9514cefdb870e1e38ce5fda680a79e6c39f7
samro01 <srozario22@gmail.com>
2022-10-20 11:58:13 +0200

    user added

--- src/Pages/News/News/News.js
+++ src/Pages/News/News/News.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import { Link, useLoaderData } from 'react-router-dom';
 
 const News = () => {
+    const news = useLoaderData();
+    const {title, details, image_url, category_id}= news;
     return (
-        <div>
-            <h3>This is news</h3>
-        </div>
+        <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={image_url}/>
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+         {details}
+        </Card.Text>
+        <Link to={`/category/${category_id}`}>
+        <Button variant="primary">All news in this Category</Button>
+        </Link>
+      </Card.Body>
+    </Card>
     );
 };
 
